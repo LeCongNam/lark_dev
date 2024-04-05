9c1a3fecd9712ee3ab9213f80299418db7af3d2a
Le Cong Nam <namlem4u@gmail.com>
2024-04-05 18:16:23 +0700

    add log

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const morgan = require("morgan");
+
+app.use(morgan("combined"));
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
